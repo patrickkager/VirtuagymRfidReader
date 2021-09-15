1772db1411d2525f8d9331b84632e9af0ac77ec1
u11d04 <patrick.kager@avl.com>
2021-09-15 08:54:23 +0200

    Init

--- VirtuagymRfidReader/Properties/AssemblyInfo.cs
+++ VirtuagymRfidReader/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("VirtuagymSync")]
+[assembly: AssemblyTitle("Virtuagym RFID Reader")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("VirtuagymSync")]
+[assembly: AssemblyProduct("VirtuagymRfidReader")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
