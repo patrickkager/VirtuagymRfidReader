c6666b6365845406b3a2fe1a8b10485f736bbdc6
Patrick Kager <patrick.kager@edu.campus02.at>
2021-10-05 09:42:51 +0200

    config change

--- VirtuagymRfidReader/Properties/Settings.Designer.cs
+++ VirtuagymRfidReader/Properties/Settings.Designer.cs
@@ -43,7 +43,7 @@ namespace VirtuagymRfidReader.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("500")]
+        [global::System.Configuration.DefaultSettingValueAttribute("750")]
         public int RepaitTimeInMs {
             get {
                 return ((int)(this["RepaitTimeInMs"]));
